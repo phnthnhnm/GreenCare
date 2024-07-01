5d2c70e4ccf63c77929ca2a75378500e921b52ef
Nam Phan <phnthnhnm0612@gmail.com>
2024-07-01 16:03:47 +0700

    chore: Refactor JavaScript code for better readability and maintainability

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,111 +1,108 @@
-const forms = document.querySelector(".forms"),
-      pwShowHide = document.querySelectorAll(".eye-icon"),
-      links = document.querySelectorAll(".link");
+const forms = document.querySelector('.forms'),
+  pwShowHide = document.querySelectorAll('.eye-icon'),
+  links = document.querySelectorAll('.link')
 
-const form = document.getElementById('form');
-const username = document.getElementById('name');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const form = document.getElementById('form')
+const username = document.getElementById('name')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-    validateInputs();
-});
+  validateInputs()
+})
 
 const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+  const inputControl = element.parentElement
+  const errorDisplay = inputControl.querySelector('.error')
 
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
+  errorDisplay.innerText = message
+  inputControl.classList.add('error')
+  inputControl.classList.remove('success')
 }
 
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+const setSuccess = (element) => {
+  const inputControl = element.parentElement
+  const errorDisplay = inputControl.querySelector('.error')
 
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-};
+  errorDisplay.innerText = ''
+  inputControl.classList.add('success')
+  inputControl.classList.remove('error')
+}
 
-const isValidEmail = email => {
-    const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    return re.test(String(email).toLowerCase());
+const isValidEmail = (email) => {
+  const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+  return re.test(String(email).toLowerCase())
 }
-const isValidName = name => {
-    const re = /^[a-z ,.'-]+$/i;
-    return re.test(String(name));
+const isValidName = (name) => {
+  const re = /^[a-z ,.'-]+$/i
+  return re.test(String(name))
 }
-const isValidPassword = password => {
-    const re = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_|\\]).{6,}$/;
-    return re.test(String(password));
+const isValidPassword = (password) => {
+  const re = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_|\\]).{6,}$/
+  return re.test(String(password))
 }
 
 const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
+  const usernameValue = username.value.trim()
+  const emailValue = email.value.trim()
+  const passwordValue = password.value.trim()
+  const password2Value = password2.value.trim()
 
-    if(usernameValue === '') {
-        setError(username, 'Username is required');
-    } else if (!isValidName(usernameValue)) {
-        setError(username, 'Provide a valid Name');
-    } 
-    else {
-        setSuccess(username);
-    }
+  if (usernameValue === '') {
+    setError(username, 'Username is required')
+  } else if (!isValidName(usernameValue)) {
+    setError(username, 'Provide a valid Name')
+  } else {
+    setSuccess(username)
+  }
 
-    if(emailValue === '') {
-        setError(email, 'Email is required');
-    } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Provide a valid email address');
-    } else {
-        setSuccess(email);
-    }
+  if (emailValue === '') {
+    setError(email, 'Email is required')
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, 'Provide a valid email address')
+  } else {
+    setSuccess(email)
+  }
 
-    if(passwordValue === '') {
-        setError(password, 'Password is required');
-    } else if (!isValidPassword(passwordValue)) {
-        setError(password, '> 6 characters. Must include uppercase and lowercase letters, a number and a special character.')
-    } else {
-        setSuccess(password);
-    }
+  if (passwordValue === '') {
+    setError(password, 'Password is required')
+  } else if (!isValidPassword(passwordValue)) {
+    setError(password, '> 6 characters. Must include uppercase and lowercase letters, a number and a special character.')
+  } else {
+    setSuccess(password)
+  }
 
-    if(password2Value === '') {
-        setError(password2, 'Please confirm your password');
-    } else if (password2Value !== passwordValue) {
-        setError(password2, "Passwords doesn't match");
-    } else {
-        setSuccess(password2);
-    }
+  if (password2Value === '') {
+    setError(password2, 'Please confirm your password')
+  } else if (password2Value !== passwordValue) {
+    setError(password2, "Passwords doesn't match")
+  } else {
+    setSuccess(password2)
+  }
+}
 
-};
+pwShowHide.forEach((eyeIcon) => {
+  eyeIcon.addEventListener('click', () => {
+    let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll('.password')
 
-pwShowHide.forEach(eyeIcon => {
-    eyeIcon.addEventListener("click", () => {
-        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-        
-        pwFields.forEach(password => {
-            if(password.type === "password"){
-                password.type = "text";
-                eyeIcon.classList.replace("bx-hide", "bx-show");
-                return;
-            }
-            password.type = "password";
-            eyeIcon.classList.replace("bx-show", "bx-hide");
-        })
-        
+    pwFields.forEach((password) => {
+      if (password.type === 'password') {
+        password.type = 'text'
+        eyeIcon.classList.replace('bx-hide', 'bx-show')
+        return
+      }
+      password.type = 'password'
+      eyeIcon.classList.replace('bx-show', 'bx-hide')
     })
-})      
+  })
+})
 
-links.forEach(link => {
-    link.addEventListener("click", e => {
-       e.preventDefault(); //preventing form submit
-       forms.classList.toggle("show-signup");
-    })
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault() //preventing form submit
+    forms.classList.toggle('show-signup')
+  })
 })
